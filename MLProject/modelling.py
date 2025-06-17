e614b01f58c1ab91d3709388e57d1db188e47d9b
NiaPutri23 <illiniamarika23@gmail.com>
2025-06-17 10:18:38 +0900

    .

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -5,30 +5,38 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import joblib 
+import argparse
 
-mlflow.set_experiment("travel_insurance_experiment")
+def main(data_path):
+    # Load data
+    df = pd.read_csv(data_path)
+    X = df.drop("TravelInsurance", axis=1)
+    y = df["TravelInsurance"]
 
-# Gunakan autolog
-mlflow.sklearn.autolog()
+    # Split data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Load data
-df = pd.read_csv("TravelInsurancePrediction_preprocessed.csv")
-X = df.drop("TravelInsurance", axis=1)
-y = df["TravelInsurance"]
+    mlflow.set_experiment("travel_insurance_experiment")
 
-# Split data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # Gunakan autolog
+    mlflow.sklearn.autolog()
 
-with mlflow.start_run():
-    model = RandomForestClassifier()
-    model.fit(X_train, y_train)
+    with mlflow.start_run():
+        model = RandomForestClassifier()
+        model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    print(f"Accuracy: {acc}")
+        y_pred = model.predict(X_test)
+        acc = accuracy_score(y_test, y_pred)
+        print(f"Accuracy: {acc}")
 
-    joblib.dump(model, "trained_model.pkl")
-    mlflow.log_artifact("trained_model.pkl")
+        joblib.dump(model, "trained_model.pkl")
+        mlflow.log_artifact("trained_model.pkl")
 
-    mlflow.log_metric("accuracy_manual", acc)
-    mlflow.sklearn.log_model(model, "model")
+        mlflow.log_metric("accuracy_manual", acc)
+        mlflow.sklearn.log_model(model, "model")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path", type=str, default="TravelInsurancePrediction_preprocessed.csv")
+    args = parser.parse_args()
+    main(args.data_path)
